8bd06613f7ec4c65311ecd6be4e8e2ba62b46374
SLX <leroux.ste@gmail.com>
2024-05-24 16:27:43 +0200

    SLX: added finalcleaning.sh

--- SRC/MAIN.sh
+++ SRC/MAIN.sh
@@ -16,8 +16,7 @@ exit 1
 source ./env_sidfex.src
 
 # take date from command-line arguments with default today
-#todayDate=$(${DATE_CMD} +\%Y\%m\%d)
-todayDate=20240523
+todayDate=$(${DATE_CMD} +\%Y\%m\%d)
 
 runDate=${1:-${todayDate}}
 
@@ -26,40 +25,40 @@ runDate=${1:-${todayDate}}
 
 # get date
 analDate=$(${DATE_CMD} +\%Y\%m\%d --date="${runDate}") #MFG
-echo ${todayDate} ${runDate} ${analDate}
 
-export ${analDate}
+echo "=== We will be working on bulletin's date : ${analDate}"
+
 source ./env_sidfex.src
 
 # SCRIPTS
 # get sidfex buoys
 #echo "######################## CURRENTLY RUNNING: get_buoy_data-auto_DKRZ.sh (GET UPDATED BUOY POSITIONS) ########################"
-bash ${DIR_SCRIPTS}/get_buoy_data-auto_DKRZ.sh ${todayDate} ${runDate} ${analDate}
+#bash ${DIR_SCRIPTS}/get_buoy_data-auto_DKRZ.sh ${todayDate} ${runDate} ${analDate}
 
 # get nextsim-f data from CMEMS
 #echo "######################## CURRENTLY RUNNING: get_nextsim_files.sh (GET NEXTISIM SEA ICEHINCAST AND FORECAST) ########################"
-bash ${DIR_SCRIPTS}get_nextsim_files.sh ${todayDate} ${runDate} ${analDate}
+#bash ${DIR_SCRIPTS}get_nextsim_files.sh ${todayDate} ${runDate} ${analDate}
 
 # generate new mesh from the nextsim-f data
 #echo "######################## CURRENTLY RUNNING: generate_arctic_mesh.sh ########################"
-bash ${DIR_SCRIPTS}generate_arctic_mesh.sh ${analDate}
+#bash ${DIR_SCRIPTS}generate_arctic_mesh.sh ${analDate}
 
 # RUN HINDCAST
 #echo "######################## CURRENTLY RUNNING: hindcast_seeds.sh (HINDCAST FOR INITIAL CONDITIONS AT MIDNIGHT)######"
-bash ${DIR_SCRIPTS}hindcast_seeds.sh ${analDate}
+#bash ${DIR_SCRIPTS}hindcast_seeds.sh ${analDate}
 
 #initiate/generate seeding
 #echo "######################## CURRENTLY RUNNING: generate_sidfex_seeding.sh ########################"
-bash ${DIR_SCRIPTS}generate_sidfex_seeding.sh ${analDate}
+#bash ${DIR_SCRIPTS}generate_sidfex_seeding.sh ${analDate}
 
 # RUN FORECAST
 # propagate seeds based on the forecasted sea ice velocities 
 #echo "######################## CURRENTLY RUNNING: propagate_sidfex_seed.sh (FORECAST) ########################"
-bash ${DIR_SCRIPTS}propagate_sidfex_seed.sh ${analDate}
+#bash ${DIR_SCRIPTS}propagate_sidfex_seed.sh ${analDate}
 
 # to sidfex - output file
 #echo "######################## CURRENTLY RUNNING: to_sidfex.sh ########################"
-bash ${DIR_SCRIPTS}to_sidfex.sh ${analDate}
+#bash ${DIR_SCRIPTS}to_sidfex.sh ${analDate}
 
 # submit outputted ascii-files to the sidfex server
 #echo "######################## CURRENTLY RUNNING: submit_file2sidfex.sh ########################"
---
+++ SRC/finalcleaningFRAZILO.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# TIME
+# get date of today 
+export analDate=$1
+echo $analDate
+
+# Extract the month (MM)
+MM=${analDate:4:2}
+
+# Extract the year (YY)
+YYYY=${analDate:0:4}
+
+source ./env_sidfex.src 
+
+# copy our sidfex forecasts from FRAZILO to SUMMER storage
+echo "copy our sidfex forecasts from FRAZILO to SUMMER storage"
+scp2CAL1 "${DIR_OUTsidfex}*.txt" /mnt/summer/DATA_MEOM/DATA_SET/SIDFEX-SeaIce-buoys/igedatlas-forecast/
+
+# copy nextsimf files from FRAZILO to SUMMER storage
+echo "copy nextsimf files from FRAZILO to SUMMER storage"
+#scp2CAL1 "${DIR_nextsim}????????_hr-nersc-MODEL-nextsimf-ARC-b${analDate}-fv00.0.nc" /mnt/summer/DATA_MEOM/DATA_SET/NEXTSIM-F/${YYYY}/${MM}/
+
+# then clean all (ask for confirmation)
+
+# Prompt for confirmation
+echo "Are you sure you want to delete the directory ${DIR_workdir}? (yes/no)"
+read confirmation
+
+# Check the user input
+if [[ "$confirmation" == "yes" ]]; then
+    # Proceed with deletion
+    rm -fr "${DIR_workdir}"
+    echo "Directory ${DIR_workdir} has been deleted."
+else
+    echo "Deletion aborted."
+fi
+
+# clean nc, npz, seed temporary directories
+cd ${DIR_sidfex}
+echo "clean"
+echo  ${DIR_sidfex}
+rm -fr ./nc ./seed ./npz ./figs ./sidfexloc.dat
+
